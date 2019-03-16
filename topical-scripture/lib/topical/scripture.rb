453a91afb7630c0dd4493e502a758f89b345c233
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-16 00:50:19 +0000

    Automatically backed up by Learn

---
+++ topical-scripture/lib/topical/scripture.rb
@@ -0,0 +1,8 @@
+require "topical/scripture/version"
+
+module Topical
+  module Scripture
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ topical-scripture/lib/topical/scripture/version.rb
@@ -0,0 +1,5 @@
+module Topical
+  module Scripture
+    VERSION = "0.1.0"
+  end
+end
